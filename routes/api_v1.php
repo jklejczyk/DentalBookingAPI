debf5353b74dab02a722fe98cc05a0d32314b1ac
Jakub Klejczyk <jklejczyk@nexmode.com>
2026-03-17 08:54:55 +0100

    dodanie testow pokrywających autentykacje

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -8,6 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// Auth
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+Route::prefix('/v1')->name('v1.')->group(function () {
+    // Auth
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+});
---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use App\Models\User;
+
+test('logowanie użytkownika', function () {
+    $user = User::factory()->create();
+
+    $response = $this->postJson(route('v1.login'),
+        [
+            'email' => $user->email,
+            'password' => 'demo123',
+        ]);
+
+    $response->assertStatus(200);
+    $response->assertJsonStructure(['data' => ['token']]);
+    $this->expect($response->json()['data']['token'])->not->toBeEmpty();
+});
+
+test('niepoprawne hasło', function () {
+    $user = User::factory()->create();
+
+    $response = $this->postJson(route('v1.login'),
+        [
+            'email' => $user->email,
+            'password' => 'asdasdasd',
+        ]);
+
+    $response->assertStatus(401);
+    $this->expect($response->json('data.token'))->toBeNull();
+});
+
+test('nieistniejący email', function () {
+    $response = $this->postJson(route('v1.login'),
+        [
+            'email' => 'losowy@email.com',
+            'password' => 'asdasdasd',
+        ]);
+
+    $response->assertStatus(401);
+    $this->expect($response->json('data.token'))->toBeNull();
+});
+
+test('brak danych logowania', function () {
+    $response = $this->postJson(route('v1.login'), []);
+
+    $response->assertStatus(422);
+    $response->assertJsonValidationErrors(['email', 'password']);
+});
+
+test('pomyśle wylogowanie', function () {
+    $user = User::factory()->create();
+    $token = $user->createToken('api test')->plainTextToken;
+
+    $response = $this->withHeader('Authorization', "Bearer $token")
+        ->postJson(route('v1.logout'));
+
+    $response->assertStatus(200);
+});
+
+test('brak tokenu przy wylogowaniu', function () {
+    $response = $this->postJson(route('v1.logout'));
+
+    $response->assertStatus(401);
+});
+
+test('niepoprawny token przy wylogowaniu', function () {
+    $response = $this->withHeader('Authorization', "Bearer 123456789")
+        ->postJson(route('v1.logout'));
+
+    $response->assertStatus(401);
+});
+
+test('token nie działa po wylogowaniu', function () {
+    $user = User::factory()->create();
+    $token = $user->createToken('api test')->plainTextToken;
+
+    $this->withHeader('Authorization', "Bearer $token")
+        ->postJson(route('v1.logout'));
+
+    $this->app['auth']->forgetGuards();
+
+    $response = $this->withHeader('Authorization', "Bearer $token")
+        ->postJson(route('v1.logout'));
+
+    $response->assertStatus(401);
+});
